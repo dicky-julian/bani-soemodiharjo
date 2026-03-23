4ca75a369dcab0c2246e7a4ddefa379965c4992d
Dicky Julian <dicky.julian@bnisekuritas.co.id>
2026-03-23 14:40:57 +0700

    feat: add animation to hero section

--- src/elements/hero/component.tsx
+++ src/elements/hero/component.tsx
@@ -1,5 +1,6 @@
 import bgHeroImg from '../../assets/bg_hero.jpg'
 import gateImg from '../../assets/gate.png'
+import flower1 from '../../assets/flower_1.png' 
 import FlowerAnimation from '../../components/flower-animation'
 import './style.css';
 
@@ -7,17 +8,25 @@ export function Hero() {
     return (
         <section className='hero'>
             <div className='hero-caption'>
-            <h1 className='text-hind'>
-                Undangan Halal Bihalal<br/>Keluarga Besar
-            </h1>
-            <h2 className='text-vibes text-primary'>Bani SoemoDiharjo</h2>
+                <h1 className='text-hind'>
+                    Undangan Halal Bihalal<br/>Keluarga Besar
+                </h1>
+                <h2 className='text-vibes text-primary'>Bani SoemoDiharjo</h2>
 
-            <h3 className='text-hind text-primary'>Idul Fitri 1447 H</h3>
-            <h3>24 Maret 2026</h3>
+                <h3 className='text-hind text-primary'>Idul Fitri 1447 H</h3>
+                <h3>24 Maret 2026</h3>
             </div>
 
             <img src={bgHeroImg} className='hero-bg-img' />
             <img src={gateImg} className='hero-gate-img' />
+
+            <div className='animation-decoration animation-decoration-left'>
+                <img src={flower1} />
+            </div>
+
+            <div className='animation-decoration animation-decoration-right'>
+                <img src={flower1} />
+            </div>
 
             <FlowerAnimation />
 
